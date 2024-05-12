b26629db7dbac2a04bdc3f1a2f0143a859987079
vo-olivier250405 <oliviervo.pro@gmail.com>
2024-05-12 17:25:02 +0200

    [ADD] => minor fix

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,7 +2,7 @@ import NavBar from "./NavBar";
 
 export default function Home() {
   return (
-    <div className="bg-orange-950 h-screen w-screeen">
+    <div className="bg-orange-950 h-screen w-screeen flex flex-row">
       <NavBar />
     </div>
   );
